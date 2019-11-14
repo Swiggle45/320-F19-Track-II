b7b29856962903348d4f307d882726eefa7fbb40
Jason Xiao <JasonX1250@gmail.com>
2019-11-14 13:45:26 -0500

    edit description now works

--- orchardwatch/src/Components/AboutUs.js
+++ orchardwatch/src/Components/AboutUs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Row, Button } from "react-bootstrap";
+import { Col, Row, Button, Form } from "react-bootstrap";
 
 class AboutUs extends React.Component {
   constructor() {
@@ -13,7 +13,7 @@ class AboutUs extends React.Component {
   componentWillMount() {
     this.setState({
       description:
-        "OrchardWatch, defender of apples, slayer of applescap, your friendly neighborhood hero!"
+        "OrchardWatch, defender of apples, slayer of applescab, your friendly neighborhood hero!"
     });
   }
 
@@ -21,39 +21,53 @@ class AboutUs extends React.Component {
     var tweets = <p>This is where the Tweets go!</p>;
     if (this.props.user === "researcher") {
       var edit = (
-        <Button
-          onClick={() => {
-            this.setState({ editting: true });
-          }}
-        >
-          Edit Description
-        </Button>
+        <div>
+          <Button
+            onClick={() => {
+              this.setState({ editting: true });
+            }}
+          >
+            Edit Description
+          </Button>
+          <br></br>
+          <br></br>
+          {this.state.description}
+        </div>
       );
       if (this.state.editting)
         edit = (
           <div>
-            <Button onClick={() => {}}>Save</Button>
-            <Button
-              onClick={() => {
-                this.setState({ editting: false });
-              }}
-            >
-              Cancel
-            </Button>
+            <Row>
+              <Col></Col>
+              <Col>
+                <Button onClick={this.updateDescription}>Save</Button>
+                <Button
+                  onClick={() => {
+                    this.setState({ editting: false });
+                  }}
+                >
+                  Cancel
+                </Button>
+                <br></br>
+                <br></br>
+                <Form>
+                  <Form.Group controlId="editDescription">
+                    <Form.Control
+                      as="textarea"
+                      rows="5"
+                      defaultValue={this.state.description}
+                    ></Form.Control>
+                  </Form.Group>
+                </Form>
+              </Col>
+              <Col></Col>
+            </Row>
           </div>
         );
       return (
         <div>
           <Row>
-            <Col md="8">
-              <Row>
-                <Col></Col>
-                <Col>{edit}</Col>
-                <Col></Col>
-              </Row>
-              <br></br>
-              <p>{this.state.description}</p>
-            </Col>
+            <Col md="8">{edit}</Col>
             <Col>{tweets}</Col>
           </Row>
         </div>
@@ -70,6 +84,11 @@ class AboutUs extends React.Component {
         </div>
       );
     }
+  }
+
+  updateDescription(event){
+    // send to lambdas
+    console.log(document.getElementById("editDescription").value);
   }
 }
 
