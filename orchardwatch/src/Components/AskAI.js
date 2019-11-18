da67401b92d3b0245bee31008083bbadc1f32cfe
Eric Teall <teall.eric@gmail.com>
2019-11-18 18:30:54 -0500

    Barebones work on ask ai

--- orchardwatch/src/Components/AskAI.js
+++ orchardwatch/src/Components/AskAI.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import {ClassDisp} from "./Classifier_Disp";
+import "./classPopQ";
+import ClassPopQ from "./classPopQ";
 
 class AskAI extends React.Component{
 
-    constructor(){
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-
+            show: false
         };
+
+        this.setModalShow = this.setModalShow.bind(this);
+    }
+
+    setModalShow(newShow){
+        this.setState({
+            ...this.state,
+            show: newShow
+        });
+        console.log(this.state);
     }
 
     render(){
         return(
             <div>
-                <p>This is the Ask AI page!</p>
-                <Button onClick={() => {}}>Submit Image</Button>
+                <ClassPopQ key={this.state.show} show={this.state.show} onHide={() => this.setModalShow(false)} />
+                <ClassDisp onClick = {() => this.setModalShow(true)} />
             </div>
         );
     }
